99a016f6935b05c5289a58ddb74402fbda99868c
woo0dev <deongho9732@gmail.com>
2022-01-05 18:16:06 +0900

    BJ1052 풀이

--- BJ1052/BJ1052/main.swift
+++ BJ1052/BJ1052/main.swift
@@ -1,29 +1,22 @@
 import Foundation
 
-func dec2binConvertor(_ decimal: Int) -> [Int] {
-    var resultArr: [Int] = []
-    var dec = decimal
-    while true {
-        resultArr.append(dec%2)
-        dec = dec / 2
-        if dec <= 1 {
-            resultArr.append(dec)
-            break
-        }
-    }
-    return resultArr
-}
-
-
-let line = readLine()!.components(separatedBy: " ")
-let n = Int(line[0])!
+let line = readLine()!.components(separatedBy: " ").map{ $0 }
+var n = Int(line[0])!
 let k = Int(line[1])!
 var count = 0
-
-for i in dec2binConvertor(n) {
-    if i == 1 {
-        count += 1
+var purchase = 0
+var bottle = n
+while true {
+    count = 0
+    bottle = n
+    while bottle != 0 {
+        count += bottle % 2
+        bottle /= 2
     }
+    if count <= k {
+        break
+    }
+    n += 1
+    purchase += 1
 }
-
-print(count)
+print(purchase)
