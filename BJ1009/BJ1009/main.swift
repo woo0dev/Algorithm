1118b8e50a027307f44c66e4d41515211f8d1292
woo0dev <deongho9732@gmail.com>
2022-01-05 11:25:30 +0900

    BJ1009 풀이

--- BJ1009/BJ1009/main.swift
+++ BJ1009/BJ1009/main.swift
@@ -1,2 +1,14 @@
 import Foundation
 
+let countInput = readLine()!
+if let count = Int(countInput.components(separatedBy: " ")[0]) {
+    var i = 0
+    while i < count {
+        let line = readLine()!.components(separatedBy: " ")
+        let a = Double(line[0])!
+        let b = Int(line[1])!
+        let data = Int(pow(a, Double(b%4))) % 10
+        data == 0 ? print(10) : print(data)
+        i += 1
+    }
+}
