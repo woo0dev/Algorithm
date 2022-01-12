c607d247f405f32468f6ad5b3e77dc1403d15dd5
woo0dev <deongho9732@gmail.com>
2022-01-12 11:47:30 +0900

    LeetCode-TwoSum 풀이

---
+++ BJ10757/BJ10757/main.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+let line = readLine()!.components(separatedBy: " ").map{ $0 }
+let a = line[0].map { $0 }
+let b = line[1].map { $0 }
+var arr1: [Int] = []
+var arr2: [Int] = []
+
+for i in a {
+    
+}
+
+print(arr1)
+
---
+++ LeetCode-TwoSum/LeetCode-TwoSum/main.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var result: [Int] = []
+        for i in 0..<nums.count {
+            for j in 1..<nums.count {
+                if nums[i] + nums[j] == target && i != j{
+                    result.append(i)
+                    result.append(j)
+                    return result
+                }
+            }
+        }
+        return result
+    }
+}
